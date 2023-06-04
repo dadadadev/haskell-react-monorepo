637b8a6ae5573addfb31a9cb3362dbd115917386
dadadadev <dadada80.dev@gmail.com>
2023-06-04 17:07:12 +0000

    call api from client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+import { useEffect, useState } from 'react'
+
+const api = () => {
+  return fetch("http://localhost:3000/hello");
+}
 
 function App() {
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const result = await api();
+      const text = await result.text();
+      setText(text);
+    })();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {text}
       </header>
     </div>
   );
