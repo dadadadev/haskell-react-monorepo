29bc392ced5e04fb2009bed5231a638ada45f190
dadadadev <dadada80.dev@gmail.com>
2023-06-04 04:37:00 +0000

    update postCreateCommand.sh for hls

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,3 +2,6 @@
 
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends dpkg-dev gcc libc6-dev libffi-dev libgmp-dev libnuma-dev libtinfo-dev zlib1g-dev
+
+# enable HLS to load stack projects in subdirectories
+stack init --force && stack build
