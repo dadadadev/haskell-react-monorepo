cbe6d7c8ca594fc62626c8984ec640d2f02542f9
dadadadev <dadada80.dev@gmail.com>
2023-06-04 05:16:23 +0000

    minimum server

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import Lib
+import Network.HTTP.Types (status200)
+import Network.Wai (Application, responseLBS)
+import Network.Wai.Handler.Warp (run)
+
+app :: Application
+app _ respond = do
+  putStrLn "minimum server running"
+  respond $
+    responseLBS
+      status200
+      [("Content-Type", "text/plain")]
+      "Hello, Web!"
 
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn $ "http://localhost:8080/"
+  run 8080 app
