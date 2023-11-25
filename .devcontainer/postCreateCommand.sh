d5c3bc11d4d784512f6c37ac00479453c0bfa927
dadadadev <dadada80.dev@gmail.com>
2023-11-25 12:21:22 +0000

    Initialize Stack in Root Directory with Specific Resolver

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,10 +3,10 @@
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends dpkg-dev gcc libc6-dev libffi-dev libgmp-dev libnuma-dev libtinfo-dev zlib1g-dev
 
-# enable HLS to load stack projects in subdirectories
+# Configure HLS for Development in Root Directory
 sudo rm -rf .stack-work
 sudo rm -f stack.yaml stack.yaml.lock
-stack init --force && stack build
+stack init --force --resolver=lts-21.21 && stack build
 
 stack install ghcid
 
